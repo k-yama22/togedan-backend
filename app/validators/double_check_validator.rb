597db5ed3d6e9b9bcdd464d74054fe14c8718a8f
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-07-23 23:50:57 +0900

    🐛 更新時に日時が変更されていない場合はバリデーションチェックをしないように修正

--- app/validators/double_check_validator.rb
+++ app/validators/double_check_validator.rb
@@ -1,8 +1,11 @@
 class DoubleCheckValidator < ActiveModel::Validator
     def validate(record)
       @check_event = Event.where(event_date: record.event_date, start_time: record.start_time.strftime('%H:%M')..record.end_time.strftime('%H:%M'),end_time: record.start_time.strftime('%H:%M')..record.end_time.strftime('%H:%M'))
-      if !@check_event.blank?
-        record.errors.add :res_message , '既に登録済みのイベントと時間が重なっているため登録できません'
+      @thisEvent = Event.find_by(id: record.id)
+      if !@thisEvent || !(record.event_date == @thisEvent.event_date && record.start_time.strftime('%H:%M') == @thisEvent.start_time.strftime('%H:%M') && record.end_time.strftime('%H:%M') == @thisEvent.end_time.strftime('%H:%M'))
+        if !@check_event.blank?
+          record.errors.add :res_message , '既に登録済みのイベントと時間が重なっているため登録できません'
+        end
       end
     end
   end

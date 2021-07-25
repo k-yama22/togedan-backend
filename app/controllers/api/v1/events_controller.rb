bf2e820fd5e91f79e505e35c1f0495e7a2f61621
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-07-25 22:59:10 +0900

    ♻️ 過去イベント一覧の取得時にユーザのアイコン画像を取得するよう修正 取得データが未来になっていたので過去に修正

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -21,8 +21,8 @@ module Api
       end
 
       def history
-        @my_events = Event.where(user_id: params[:id])
-        @my_history = @my_events.where(event_date: Date.today..)
+        @my_events = User.joins(:events).select("users.image,users.id, events.id AS event_id,events.event_name,events.genre,events.location,events.event_date,events.start_time,events.end_time,events.event_message,events.max_people").where(events: {user_id: params[:id]})
+        @my_history = @my_events.where(events: {event_date: ..Date.today})
         render json: { status: 200, message: 'Loaded the my events', data: @my_history }
       end
 

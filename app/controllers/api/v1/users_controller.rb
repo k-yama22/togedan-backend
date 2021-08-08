595ce92dae001e150da5c7cfc6b78d5cd47afd06
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-08-08 22:30:08 +0900

    ✨ ユーザーコントローラに認証処理を追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_user!
       def show
         @user = User.find(params[:id])
         render json: { status: 200, message: 'ユーザ情報を取得しました', data: @user }

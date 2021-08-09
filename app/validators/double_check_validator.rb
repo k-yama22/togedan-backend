f672c4d269cdbe3d63fb843f3dc1aa4df5a1fb54
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-08-09 23:35:18 +0900

    🐛 不具合の修正

--- app/validators/double_check_validator.rb
+++ app/validators/double_check_validator.rb
@@ -1,6 +1,6 @@
 class DoubleCheckValidator < ActiveModel::Validator
     def validate(record)
-      @check_event = Event.where('event_date = ? and end_time > ? and ? > start_time', record.event_date, record.start_time.strftime('%H:%M'), record.end_time.strftime('%H:%M'))
+      @check_event = Event.where('user_id = ? and event_date = ? and end_time > ? and ? > start_time', record.user_id, record.event_date, record.start_time.strftime('%H:%M'), record.end_time.strftime('%H:%M'))
       @thisEvent = Event.find_by(id: record.id)
       if !@thisEvent || !(record.event_date == @thisEvent.event_date && record.start_time.strftime('%H:%M') == @thisEvent.start_time.strftime('%H:%M') && record.end_time.strftime('%H:%M') == @thisEvent.end_time.strftime('%H:%M'))
         if !@check_event.blank?
